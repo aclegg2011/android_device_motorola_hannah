e25b8a7a62e685ad4d890771e0bce1e3c990fa61
Aren Clegg <aclegg2011@gmail.com>
2019-06-11 16:37:06 +0000

    [hannah] update kernel flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -122,15 +122,20 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/hidl/compatibility_matrix.xml
 #TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8937
 
 # Kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 vmalloc=350M
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
-BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+#BOARD_KERNEL_SEPARATED_DT := true
+#TARGET_CUSTOM_DTBTOOL := dtbTool_custom
+#BOARD_DTBTOOL_ARGS := --force-v3 --motorola 1
+#BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_HEADER := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := hannah_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8937
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
